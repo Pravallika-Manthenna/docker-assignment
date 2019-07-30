67154470ab4a8009b7889a111d53aa74f982f8ee
Pravallika-Manthenna <mslpravallika6@gmail.com>
2019-07-30 15:27:42 +0530

    docker task

---
+++ src/main/java/com/stackroute/MuzixAppMysql/domain/Track.java
@@ -0,0 +1,149 @@
+//package com.stackroute.MuzixAppMysql.domain;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//@EntityListeners(AuditingEntityListener.class)
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+//
+//    private String name;
+//
+//    private String artist;
+//
+//    private String url;
+//
+//    private String streamable;
+//
+//    private int listeners;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//}
+package com.stackroute.MuzixAppMysql.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.*;
+
+//@Entity
+//@Table(name = "Track")
+@Data
+@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Document
+public class Track {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private String artist;
+
+    private String url;
+
+    private String streamable;
+
+    private int listeners;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    private String test;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStreamable() {
+        return streamable;
+    }
+
+    public void setStreamable(String streamable) {
+        this.streamable = streamable;
+    }
+
+    public int getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(int listeners) {
+        this.listeners = listeners;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Track(int id, String name, String artist, String url, String streamable, int listeners) {
+        this.id = id;
+        this.name = name;
+        this.artist = artist;
+        this.url = url;
+        this.streamable = streamable;
+        this.listeners = listeners;
+    }
+}
